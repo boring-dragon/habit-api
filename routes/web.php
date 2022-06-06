733a0362177a9b9d95cf407a7aefc6b05e418246
Mohamed jinas <j@live.mv>
2022-06-06 08:44:30 +0500

    WIP

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response->json("Welcome to habbit api");
+    return response()->json("Welcome to habbit api");
 });
