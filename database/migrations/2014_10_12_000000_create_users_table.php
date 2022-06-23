6074a67b924172dc128b4baf081b6ff0de39276b
Mohamed jinas <j@live.mv>
2022-06-23 07:54:43 +0500

    WIP

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->text('bio')->nullable();
             $table->string('email')->unique();
+            $table->string('type')->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
