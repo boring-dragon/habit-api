d02709353c7cb9d0cb396702c1b4d59c86b02b12
Mohamed jinas <j@live.mv>
2022-06-30 07:41:17 +0500

    WIP

--- app/Models/Habbit.php
+++ app/Models/Habbit.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Habbit extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'habbit_type_id',
+        'habbit_name',
+        'status'
+    ];
 }
--- app/Models/HabbitType.php
+++ app/Models/HabbitType.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class HabbitType extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
 }
