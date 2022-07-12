8b759582eb000b0c722696a61c62b80dfa7c4d5c
Mohamed jinas <j@live.mv>
2022-07-12 05:39:30 +0500

    WIP

---
+++ app/Http/Controllers/API/CharacterController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Character;
+use Illuminate\Http\Request;
+
+class CharacterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Character  $character
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Character $character)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Character  $character
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Character $character)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Character  $character
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Character $character)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Character  $character
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Character $character)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/API/HabbitTypeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\HabbitType;
+use Illuminate\Http\Request;
+
+class HabbitTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HabbitType  $habbitType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HabbitType $habbitType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HabbitType  $habbitType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HabbitType $habbitType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HabbitType  $habbitType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HabbitType $habbitType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HabbitType  $habbitType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HabbitType $habbitType)
+    {
+        //
+    }
+}
