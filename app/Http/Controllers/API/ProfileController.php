e8abe459ba946100ab7b463b88f372631af5a9d7
Mohamed jinas <j@live.mv>
2022-07-12 05:51:10 +0500

    wallet and profile

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function show(User $user)
+    {
+        return response()->json([
+            'message' => 'Profile retrieved successfully',
+            'data' => $user,
+        ], 200);
+    }
+
+    public function update()
+    {
+        // Handle the user profile update
+    }
 }
--- app/Http/Controllers/API/WalletController.php
+++ app/Http/Controllers/API/WalletController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 
 class WalletController extends Controller
 {
-    //
+    public function show(Wallet $wallet)
+    {
+        return response()->json([
+            'message' => 'Wallet retrieved successfully',
+            'data' => $wallet,
+        ], 200);
+    }
+
+    public function update(Request $request, Wallet $wallet)
+    {
+        $request->validate([
+            'amount' => 'required|numeric',
+        ]);
+
+        $wallet->update($request->all());
+
+        return response()->json([
+            'message' => 'Wallet updated successfully',
+            'data' => $wallet,
+        ], 200);
+    }
 }
